74c3b730d0ef24beaad89ec8600785281859994c
harsh9539 <goyalh862@gmail.com>
2022-04-20 07:00:34 +0530

    index of first using Iterative approach

--- Searching/Practise/index_of_first_occ.cpp
+++ Searching/Practise/index_of_first_occ.cpp
@@ -11,32 +11,48 @@ using namespace std;
 //         }
 // };
 
-class Recursive{
+// class Recursive{
+//     public:
+//         int firstOcc(int arr[],int low,int high ,int k){
+//             if(low>high) return -1;
+//             int mid = (low+high)/2;
+//             if(arr[mid]<k) return firstOcc(arr, mid+1, high, k);
+//             else if(arr[mid]> k) return firstOcc(arr, low, (mid-1), k);
+//             else{
+//                 if(mid == 0 || arr[mid-1] != arr[mid]) return mid;
+//                 else return firstOcc(arr,low,(mid-1),k);
+//             }
+//         }
+// };
+
+class Iterative{
     public:
-        int firstOcc(int arr[],int low,int high ,int k){
-            if(low>high) return -1;
-            int mid = (low+high)/2;
-            if(arr[mid]<k) return firstOcc(arr, mid+1, high, k);
-            else if(arr[mid]> k) return firstOcc(arr, low, (mid-1), k);
-            else{
+        int firstOcc(int arr[],int n,int k){
+            int low = 0,high = n-1;
+            while(low<=high){
+                int mid = (low+high)/2;
+                if(arr[mid]<k) low=mid+1;
+                else if(arr[mid] > k) high = mid-1;
+                else{
                 if(mid == 0 || arr[mid-1] != arr[mid]) return mid;
-                else return firstOcc(arr,low,(mid-1),k);
+                else high = mid-1;
             }
+            }
+        return -1;
         }
 };
-
 int main(){
-    int n,k,low=0;
+    int n,k;
     cin >> n>> k;
-    int high = n-1;
     int arr[n];
     for (int  i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
     // Naive sol;
-    Recursive sol;
-    int ans = sol.firstOcc(arr,low,high,k);
+    // Recursive sol;
+    Iterative sol;
+    int ans = sol.firstOcc(arr,n,k);
     cout << ans;
     return 0;
 }
