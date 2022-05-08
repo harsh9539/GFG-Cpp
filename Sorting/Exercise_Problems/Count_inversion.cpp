5e3423f024c73e2f1cd3f045338c08cb75f3ed32
harsh9539 <goyalh862@gmail.com>
2022-05-08 10:40:17 +0530

    count inversion

---
+++ Sorting/Exercise_Problems/Count_inversion.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    int res = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            cout << arr[j];
+            if(arr[i]> arr[j]) {res++;}
+        }
+        cout << endl;
+        
+    }
+    cout << res;
+    
+    return 0;
+}
