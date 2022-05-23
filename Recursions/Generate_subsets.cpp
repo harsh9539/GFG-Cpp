49833965c79a8be682c49d04ece6a5869193a9b5
harsh9539 <goyalh862@gmail.com>
2022-05-23 11:06:18 +0530

    generate subsets of string

---
+++ Recursions/Generate_subsets.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void subsets(string str, string curr = "", int ind = 0){
+    if(ind == str.length()){
+        cout << curr<<" ";
+        return ;
+    }
+    subsets(str,curr,ind+1);
+    subsets(str,curr+str[ind],ind+1);
+}
+
+int main(){
+    string str = "harsh";
+    subsets(str);
+    
+    return 0;
+}
