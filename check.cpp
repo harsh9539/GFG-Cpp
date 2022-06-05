f6ae4c0b3f835bd51775b76905bacb6b29b7bb62
harsh9539 <goyalh862@gmail.com>
2022-06-05 16:05:53 +0530

    #define

---
+++ check.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define maxi(x,y) (x>y?x:y)
+#define msg(x) #x
+#define pi 3.1425
+#ifndef pie
+    #define pie 3.414
+#endif
+
+int main(){
+    cout << pi << endl;
+    cout << maxi(10,12) << endl;
+    cout << msg(hello) << endl;
+    vector<int> myvec{1,2,3,4,5};
+    cout << myvec.size();
+    return 0;
+}
