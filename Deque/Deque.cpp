4f847e240742ba1a361c455bf76cd02a6a58794a
harsh9539 <goyalh862@gmail.com>
2022-08-03 09:35:09 +0530

    O(1) implementation using array

--- Deque/Deque.cpp
+++ Deque/Deque.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 struct Deque{
-	int size,cap,*arr;
+	int size,cap,*arr,front;
 	Deque(int c){
 		cap = c;
+		front = 0;
 		size = 0;
 		arr = new int[c];
 	}
@@ -11,7 +12,9 @@ struct Deque{
 	bool isEmpty(){return (size == 0);}
 	void insertRear(int x){
 		if(isFull()) return ;
-		arr[size] = x;
+		int new_rear = (front+size)%cap;
+		arr[new_rear] = x;
+		// arr[size] = x;
 		size++;
 	}
 	void deleteRear(int x){
@@ -19,39 +22,36 @@ struct Deque{
 		size--;
 	}
 	int getRear(){
-		int val =  arr[size-1];
+		// int val =  arr[size-1];
 		if(isEmpty()) return -1;
-		else return val;
+		else return arr[(front+size-1)%cap];
 	}
 	void insertFront(int x){
 		if(isFull()) return;
-		for (int i = size-1;i>=0; i--)
-		{
-			arr[i+1] = arr[i];
-		}
+		// for (int i = size-1;i>=0; i--)
+		// {
+		// 	arr[i+1] = arr[i];
+		// }
 		
-		arr[0] = x;
+		// arr[0] = x;
+		front = (front+cap-1)%cap;
+		arr[front] = x;
 		size++;
 	}
 	void deleteFront(){
 		if(isEmpty()) return;
-		for (int i = 0; i < size-1; ++i)
-		{
-			arr[i] = arr[i+1];
-		}
+		// for (int i = 0; i < size-1; ++i)
+		// {
+		// 	arr[i] = arr[i+1];
+		// }
+		front = (front+1)%cap;
 		size--;
 	}
 	int getFront(){
 		if(isEmpty()) return -1;
-		else return arr[0];
+		else return arr[front];
 	}
-	void display(){
-		for (int i = 0; i < size; ++i)
-		{
-			cout << arr[i] << " ";
-		}
-		cout << endl;
-	}
+
 };
 int main(){
 	int n;
@@ -65,11 +65,15 @@ int main(){
 	}
 	// d.insertRear(10);
 	// d.insertRear(20);
-	cout << d.getRear()<<endl;
-	cout << d.getFront() << endl;
+	// cout << d.getRear()<<endl;
+	// cout << d.getFront() << endl;
 	d.display();
-	cout << d.isFull() << endl;
-	cout << d.isEmpty() << endl;
+	d.deleteFront();
+	d.insertRear(80);
+	d.display();
+
+	// cout << d.isFull() << endl;
+	// cout << d.isEmpty() << endl;
 
 	
 	return 0;
