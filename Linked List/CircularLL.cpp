b988cd35245c276eef976e0f96c5a30d9acc75c6
harsh9539 <goyalh862@gmail.com>
2022-08-09 14:25:18 +0530

    deleting in circular LL

--- Linked List/CircularLL.cpp
+++ Linked List/CircularLL.cpp
@@ -62,6 +62,16 @@ int serachLL(Node *head,int d){
     return -1;
 }
 
+// deleting in circular LL 
+Node *deleteHead(Node *head){
+    if(head == NULL) return NULL;
+    if(head->next == head) {delete head;return NULL;}
+    head->data = head->next->data;
+    Node *temp = head->next;
+    head->next = head->next->next;
+    delete temp;
+    return head;
+}
 
 void printLL(Node *head){
     if(head == NULL) {cout << "Linked List is Empty";return;}
@@ -75,11 +85,13 @@ void printLL(Node *head){
 
 int main(){
     Node *head = NULL;
-    head = insertEnd(head,30);
     head = insertEnd(head,20);
+    head = insertEnd(head,30);
     head = insertEnd(head,40);
     head = insertEnd(head,50);
     printLL(head);
-    cout << endl << serachLL(head,60);
+    deleteHead(head);cout << endl;
+    printLL(head);
+    cout << endl << serachLL(head,40);
     return 0;
 }
