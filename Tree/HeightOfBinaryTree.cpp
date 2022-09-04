9896e4465fe8f5091816f5229325fa6195076301
harsh9539 <goyalh862@gmail.com>
2022-09-04 11:26:30 +0530

    height of binary Tree

---
+++ Tree/HeightOfBinaryTree.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int key;
+    Node *right,*left;
+    Node(int val){
+        key = val;
+        left = right = NULL;
+    }
+};
+
+int heightTree(Node *root){
+    if(root == NULL) return 0;
+    else return max(heightTree(root->left),heightTree(root->right))+1;
+}
+
+
+int main(){
+    Node *root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->left->left = new Node(40);
+    root->left->right = new Node(25);
+    cout << heightTree(root);
+    return 0;
+}
