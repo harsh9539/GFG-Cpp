6d5397f05f1857daa1708836945519882508fc66
harsh9539 <goyalh862@gmail.com>
2022-09-05 18:28:21 +0530

    check for balanced tree

---
+++ Tree/BalancedTree.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int key;
+    Node *right,*left;
+    Node(int val){
+        key = val;
+        left = right = NULL;
+    }
+};
+
+int isBalanced(Node *root)
+{
+    if(root == NULL) return 0;
+    int lh = isBalanced(root->left);
+    if(lh == -1) return -1;
+    int rh = isBalanced(root->right);
+    if(rh == -1) return -1;
+    if(abs(lh-rh)>1) return -1;
+    else return max(lh,rh)+1;
+}
+
+
+int main(){
+    Node *root = new Node(20);
+    root->left = new Node(8);
+    root->right = new Node(12);
+    root->left->left = new Node(3);
+    root->left->right = new Node(5);
+    // root->left->left->left = new Node(28);
+    // root->right->right = new Node(50);
+    int ans = isBalanced(root);
+    if(ans != -1) cout << "Balanced";
+    else cout << "Not Balanced";
+    return 0;
+}
