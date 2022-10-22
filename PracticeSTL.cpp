d7bfd096e6f21be2639f1766ce6619e67f40b178
harsh9539 <goyalh862@gmail.com>
2022-10-22 14:44:36 +0530

    Practice Vectors with Pairs

---
+++ PracticeSTL.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    // Pairs
+    /*
+    pair<int,int> pr[3];
+    for(int i = 0;i<3;i++){
+        pr[i] = {i,i+5};
+    }
+    for(int i = 0;i<3;i++){
+        cout << pr[i].first << " " << pr[i].second<< endl;
+    }
+    */
+
+    // vector of Pairs
+    /*
+    vector<pair<int,int>> vp;
+    for (int i = 0; i < 3; i++)
+    {
+        vp.push_back({i,i+5});
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        // vp.push_back({i,i+4});
+        cout << vp[i].first << ' ' << vp[i].second << endl;
+    }
+    */
+
+    // Vector of Vectors
+    /*
+    int val = 0;
+    vector<vector<int>> vv;
+    for (int i = 0; i < 3; i++)
+    {
+        vector<int> v;
+        for (int j = 0; j < 3; j++)
+        {
+            v.push_back(val);
+            val++;
+        }
+        vv.push_back(v);
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << vv[i][j] << " ";
+        }
+        cout << endl;
+    }
+    */
+
+    // Vector of pairs of vectors
+    int val = 1;
+    vector<pair<int, vector<int>>> vpv;
+    for (int i = 0; i < 3; i++)
+    {
+        vector<int> v;
+        for (int j = 0; j < 3; j++)
+        {
+            v.push_back(val);
+            val++;
+        }
+        vpv.push_back({i, v});
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << vpv[i].first << "->";
+        for (int j = 0; j < 3; j++)
+        {
+            cout << vpv[i].second[j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
