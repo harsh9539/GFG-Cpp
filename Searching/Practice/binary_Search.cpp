4d1fd3a00be605e040a0146b092581a2d22f0327
harsh9539 <goyalh862@gmail.com>
2022-11-03 12:24:43 +0530

    binary search

--- Searching/Practice/binary_Search.cpp
+++ Searching/Practice/binary_Search.cpp
@@ -1,20 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// class iterative{
-//     public:
-//         int bsearch(int arr[],int n,int k){
-//             int low = 0,high = n-1;
-//             cout << "value of n is : "<< k<<endl;
-//             while(low<=high){
-//                 int mid = (low+high)/2;
-//                 if(arr[mid]==k) return mid;
-//                 else if(arr[mid] > k) high = mid-1;
-//                 else low = mid + 1;
-//             }
-//         return -1;
-//         }
-// };
+class iterative{
+    public:
+        int bsearch(int arr[],int n,int k){
+            int low = 0,high = n-1;
+            while (low<=high)
+            {
+                int mid = (low+high)/2;
+                if(arr[mid] == k) {return mid; break;}
+                else if(arr[mid] > k){
+                    high = mid -1;
+                }
+                else{
+                    low = mid + 1;
+                }
+                cout << "arr["<< mid << "]: " << arr[mid] << endl;
+            }
+            return -1;
+        }
+};
 class Recursive{
     public:
         int bsearch(int arr[],int low,int high,int k){
@@ -28,16 +33,18 @@ class Recursive{
 
 int main(){
     int n,k,low=0;
-    cin >> n>>k;
+    cin >> n;
     int high = n-1;
     int arr[n];
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-    // iterative sol;
-    Recursive sol;
-    int ans = sol.bsearch(arr,low,high,k);
+    cin >>k;
+    iterative sol;
+    int ans = sol.bsearch(arr,n,k);
+    // Recursive sol;
+    // int ans = sol.bsearch(arr,low,high,k);
     cout << ans;
     return 0;
 }
