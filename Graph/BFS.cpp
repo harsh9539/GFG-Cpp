104b9cf61c9cd7f0b63b0365ad047a0a98866f00
harsh9539 <goyalh862@gmail.com>
2023-02-15 18:33:14 +0530

    BFS when no source is given and even if our graph is broken

--- Graph/BFS.cpp
+++ Graph/BFS.cpp
@@ -16,10 +16,29 @@ void print_graph(vector<int> adj[],int v){
     
 }
 
-void BFS(vector<int> adj[],int v,int s){
-    // source is given in this 
-    bool visited[v+1];
-    for (int i = 0; i < v; i++) visited[i] = false;
+// void BFS(vector<int> adj[],int v,int s){
+//     // source is given in this 
+//     bool visited[v+1];
+//     for (int i = 0; i < v; i++) visited[i] = false;
+//     queue<int> q;
+//     visited[s] = true;
+//     q.push(s);
+//     while (!q.empty())
+//     {
+//         int u = q.front();
+//         q.pop();
+//         cout << u << " ";
+//         for(auto x:adj[u]){
+//             if(visited[x] == false){
+//             visited[x] = true;
+//             q.push(x);
+//             }
+//         }
+//     }
+// }
+
+void BFS(vector<int> adj[],int s,bool visited[]){
+    // when no source is given and even if our graph is broken
     queue<int> q;
     visited[s] = true;
     q.push(s);
@@ -37,18 +56,29 @@ void BFS(vector<int> adj[],int v,int s){
     }
 }
 
+void BFSdis(vector<int> adj[],int v){
+    bool visited[v+1];
+    for (int i = 0; i < v; i++) visited[i] = false;
+    for (int i = 0; i < v; i++)
+    {
+        if(visited[i] == false) BFS(adj,i,visited);
+    }
+    
+}
+
 
 int main(){
-    int v = 5;
+    int v = 6;
     vector<int>  adj[v];
     addEdge(adj,0,1);
     addEdge(adj,0,2);
     addEdge(adj,1,2);
     addEdge(adj,1,3);
     addEdge(adj,2,3);
-    addEdge(adj,2,4);
-    addEdge(adj,3,4);
-    BFS(adj,v,0);
+    // addEdge(adj,2,4);
+    // addEdge(adj,3,4);
+    addEdge(adj,4,5);
+    BFSdis(adj,v);
     // print_graph(adj,v);
     return 0;
 }
